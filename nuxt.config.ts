cebb302884ddc5be7401225920c3a990131048d2
Moeein Aali <moeeeinaali@gmail.com>
2024-12-12 13:07:27 +0330

    feat: add Vuetify

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,13 +1,34 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
+import vuetify from 'vite-plugin-vuetify';
+
 export default defineNuxtConfig({
     compatibilityDate: '2024-11-01',
     devtools: {enabled: true},
-    css: ['~/assets/css/main.css'],
+
+    css: [
+        '~/assets/css/main.css',
+        'vuetify/styles',
+        '@mdi/font/css/materialdesignicons.min.css',
+    ],
+
     postcss: {
         plugins: {
             tailwindcss: {},
             autoprefixer: {},
         },
     },
-})
 
+    vite: {
+        plugins: [vuetify()],
+    },
+
+    build: {
+        transpile: ['vuetify'],
+    },
+
+    runtimeConfig: {
+        public: {
+            theme: 'light',
+        },
+    },
+});

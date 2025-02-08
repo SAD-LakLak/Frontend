dfaa0c1f399ea4ab7ec41b06bbfb5b473c27b258
mahyazz <mahya.zarrabi@gmail.com>
2025-02-08 04:36:30 +0330

    feat: add banners

--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -1,23 +1,46 @@
 import React from "react";
 import Header from "./Header.tsx";
 import Footer from "./Footer.tsx";
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import {Link} from "react-router-dom";
+
+const settings = {
+    dots: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    infinite: true,
+    autoplay: true,
+    autoplaySpeed: 3000,
+};
 
 function Home({children}: { children: React.ReactNode }) {
     return (
-        <div className="flex flex-col px-16 py-4 gap-4 bg-primaryLight h-full justify-between relative">
+        <div className="flex flex-col px-16 py-4 gap-4 bg-primaryLight h-auto justify-between relative">
             <Header/>
 
-            {/* تصویر زمینه */}
-            <img
-                className="flex-grow h-full object-cover rounded-2xl"
-                src="/images/commingsoon.png"
-                alt="Coming Soon"
-            />
+            {/* Banners */}
+            <Slider {...settings}>
+                <Link to="/packages">
+                    <div>
+                    <img className="flex-grow h-full object-cover rounded-2xl"
+                    src="/images/banner1.png"
+                    alt="Banner 1"/>
+                    </div>
+                </Link>
+                <Link to="localhost:5173">
+                    <div>
+                    <img className="flex-grow h-full object-cover rounded-2xl"
+                    src="/images/banner2.png"
+                    alt="Banner 2"/>
+                    </div>
+                </Link>
+            </Slider>
 
-            {/* محتوا وسط صفحه */}
-            <div className="absolute inset-0 mt-24 mb-20 mx-16 flex justify-center items-center z-10">
-                {children}
-            </div>
+            {/* Recent Packages */}
+            
 
             <Footer/>
         </div>
